dab76ef22edf4fdbfab6615dd16008bf7ed4201f
Сергей Трофимов <sm.trofimov2004@gmail.com>
2023-05-14 23:48:07 +0300

    14.05.2023

--- AVLTree/AVLTree.cs
+++ AVLTree/AVLTree.cs
@@ -8,7 +8,7 @@ namespace AVLTree
 {
     public class AVLTree<T> : IEnumerable<T> where T : IComparable
     {
-        public long Iterations;
+        public int Iterations;
         public AVLTreeNode<T> Head
         {
             get;
@@ -22,41 +22,48 @@ namespace AVLTree
 
         public void Add(T value)
         {
+            // Вариант 1:  Дерево пустое - создание корня дерева      
             if (Head == null)
             {
                 Iterations++;
                 Head = new AVLTreeNode<T>(value, null, this);
             }
-
+            // Вариант 2: Дерево не пустое - найти место для добавление нового узла.
             else
-            {
                 AddTo(Head, value);
-            }
 
             Count++;
         }
         private void AddTo(AVLTreeNode<T> node, T value)
         {
+            // Вариант 1: Добавление нового узла в дерево. Значение добавлемого узла меньше чем значение текущего узла.      
             Iterations++;
             if (value.CompareTo(node.Value) < 0)
             {
+                //Создание левого узла, если его нет.
                 if (node.Left == null)
                 {
                     node.Left = new AVLTreeNode<T>(value, node, this);
                 }
-
                 else
+                {
+                    // Переходим к следующему левому узлу
                     AddTo(node.Left, value);
+                }
             }
+            // Вариант 2: Добавлемое значение больше или равно текущему значению.
             else
             {
+                //Создание правого узла, если его нет.         
                 if (node.Right == null)
                 {
                     node.Right = new AVLTreeNode<T>(value, node, this);
                 }
-
                 else
+                {
+                    // Переход к следующему правому узлу.             
                     AddTo(node.Right, value);
+                }
             }
         }
         public bool Contains(T value)
@@ -74,20 +81,27 @@ namespace AVLTree
 
         public AVLTreeNode<T> Find(T value)
         {
-            AVLTreeNode<T> current = Head; 
-            while (current != null)
+            AVLTreeNode<T> current = Head; // помещаем текущий элемент в корень дерева
+         
+            while (current != null)   // Пока текщий узел не пустой 
             {
                 Iterations++;
-                int result = current.CompareTo(value);
-
+                int result = current.CompareTo(value); // сравнение значения текущего элемента с искомым значением
                 if (result > 0)
+                {
+                    // Если значение меньше текущего - переход влево 
                     current = current.Left;
-
+                }
                 else if (result < 0)
+                {
+                    // Если значение больше текщего - переход вправо             
                     current = current.Right;
-
+                }
                 else
+                {
+                    // Элемент найден      
                     break;
+                }
             }
             return current;
         }
@@ -95,39 +109,46 @@ namespace AVLTree
         {
             Iterations++;
             AVLTreeNode<T> current;
-            current = Find(value);
+            current = Find(value); // находим узел с удаляемым значением
 
-            if (current == null) 
+            if (current == null) // узел не найден
                 return false;
 
-            AVLTreeNode<T> treeToBalance = current.Parent; 
-            Count--;
-            
-            if (current.Right == null) 
+            AVLTreeNode<T> treeToBalance = current.Parent; // баланс дерева относительно узла родителя
+            Count--; // уменьшение количества узлов
+
+            // Вариант 1: Если удаляемый узел не имеет правого потомка
+            if (current.Right == null) // если нет правого потомка
             {
-                if (current.Parent == null)
+                if (current.Parent == null) // удаляемый узел является корнем
                 {
-                    Head = current.Left;  
+                    Head = current.Left;  // на место корня перемещаем левого потомка
 
                     if (Head != null)
-                        Head.Parent = null; 
+                        Head.Parent = null;  // убираем ссылку на родителя  
                 }
-                else 
+                else // удаляемый узел не является корнем
                 {
                     int result = current.Parent.CompareTo(current.Value);
 
-                    if (result > 0)
+                    if (result > 0)  
+                        // Если значение родительского узла больше значения удаляемого,
+                        // сделать левого потомка удаляемого узла, левым потомком родителя.  
                         current.Parent.Left = current.Left;
 
                     else if (result < 0)
+                        // Если значение родительского узла меньше чем удаляемого,                 
+                        // сделать левого потомка удаляемого узла - правым потомком родительского узла.        
                         current.Parent.Right = current.Left;
                 }
             }
-            else if (current.Right.Left == null) 
+            // Вариант 2: Если правый потомок удаляемого узла не имеет левого потомка,
+            // тогда правый потомок удаляемого узла становится потомком родительского узла.      
+            else if (current.Right.Left == null) // если у правого потомка нет левого потомка
             {
                 current.Right.Left = current.Left;
 
-                if (current.Parent == null)
+                if (current.Parent == null) // текущий элемент является корнем
                 {
                     Head = current.Right;
 
@@ -137,24 +158,33 @@ namespace AVLTree
                 else
                 {
                     int result = current.Parent.CompareTo(current.Value);
-
+        
                     if (result > 0)
+                        // Если значение узла родителя больше чем значение удаляемого узла,                 
+                        // сделать правого потомка удаляемого узла, левым потомком его родителя.      
                         current.Parent.Left = current.Right;
 
                     else if (result < 0)
+                        // Если значение родительского узла меньше значения удаляемого,                 
+                        // сделать правого потомка удаляемого узла - правым потомком родителя.                 
                         current.Parent.Right = current.Right;
                 }
             }
+
+            // Вариант 3: Если правый потомок удаляемого узла имеет левого потомка,      
+            // заменить удаляемый узел крайним левым потомком правого потомка.     
             else
             {
+                // Нахождение крайнего левого узла для правого потомка удаляемого узла.       
                 AVLTreeNode<T> leftmost = current.Right.Left;
 
                 while (leftmost.Left != null)
                 {
                     leftmost = leftmost.Left;
                 }
-
+                // Родительское правое поддерево становится родительским левым поддеревом.         
                 leftmost.Parent.Left = leftmost.Right;
+                // Присвоить крайнему левому узлу, ссылки на правого и левого потомка удаляемого узла.       
                 leftmost.Left = current.Left;
                 leftmost.Right = current.Right;
 
@@ -168,9 +198,13 @@ namespace AVLTree
                 {
                     int result = current.Parent.CompareTo(current.Value);
 
+                    // Если значение родительского узла больше значения удаляемого,                 
+                    // сделать крайнего левого потомка левым потомком родителя удаляемого узла.                 
                     if (result > 0)
                         current.Parent.Left = leftmost;
 
+                    // Если значение родительского узла, меньше чем значение удаляемого,                 
+                    // сделать крайнего левого потомка, правым потомком родителя удаляемого узла.                 
                     else if (result < 0)
                         current.Parent.Right = leftmost;
                 }
--- AVLTree/Program.cs
+++ AVLTree/Program.cs
@@ -9,50 +9,43 @@ namespace AVLTree
     {
         static void Main(string[] args)
         {
-            List<int> avg = new List<int>();
             AVLTree<int> tree = new AVLTree<int>();
             int[] array = WorkTools.GenerateRandomArray(10000);
 
-            // Add elements to the tree
             Stopwatch sw = new Stopwatch();
             for (int i = 0; i < array.Length; i++)
             {
                 sw.Start();
                 tree.Add(array[i]);
-                sw.Stop();  
-                string resultString = ($"Вставка {array[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
-                avg.Add(resultString);
+                sw.Stop();
+                Console.WriteLine($"Вставка {array[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
                 sw.Reset();
                 tree.Iterations = 0;
             }
 
-            // Search for random elements in the tree
             int[] searchKeys = WorkTools.GetRandomSubarray(array, 100);
             for (int i = 0; i < searchKeys.Length; i++)
             {
                 sw.Start();
                 AVLTreeNode<int> node = tree.Find(searchKeys[i]);
                 sw.Stop();
-                string resultString = ($"Поиск {searchKeys[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
-                avg.Add(resultString);
+                Console.WriteLine($"Поиск {searchKeys[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
                 sw.Reset();
                 tree.Iterations = 0;
             }
 
-            //  Remove random elements from the tree
             int[] removeKeys = WorkTools.GetRandomSubarray(array, 1000);
             for (int i = 0; i < removeKeys.Length; i++)
             {
                 sw.Start();
                 tree.Remove(removeKeys[i]);
                 sw.Stop();
-                string resultString = ($"Удаление {removeKeys[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
-                avg.Add(resultString);
+                Console.WriteLine($"Удаление {removeKeys[i]}: время = {sw.ElapsedTicks}, операций = {tree.Iterations}");
                 sw.Reset();
                 tree.Iterations = 0;
             }
         }
 
-       
+
     }
 }
